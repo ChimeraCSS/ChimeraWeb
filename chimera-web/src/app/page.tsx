a789eccba2e1819c4e6f4d7ee7646f3168dae631
Markus A. R. Johansen <90006516+J0hans1@users.noreply.github.com>
2023-08-15 22:22:10 +0200

    Initial Commit

---
+++ chimera-web/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() { 
+  return (
+    <main className='flex justify-center'>
+      <h1>This is <a>ChimeraCSS</a></h1>
+    </main>
+  )
+}
