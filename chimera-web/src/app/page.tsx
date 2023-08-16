c1e250c7e4ffb5f98a0f4b7a5b83fe49a156a46e
Markus A. R. Johansen <90006516+J0hans1@users.noreply.github.com>
2023-08-16 17:30:06 +0200

    navbar

--- chimera-web/src/app/page.tsx
+++ chimera-web/src/app/page.tsx
@@ -2,8 +2,24 @@ import Image from 'next/image'
 
 export default function Home() { 
   return (
-    <main className='flex justify-center'>
-      <h1>This is <a>ChimeraCSS</a></h1>
-    </main>
+    <>
+      <header className='bg-gray-200 flex justify-between p-5'>
+        <img src='favicon.ico' className='h-10'/>
+        {/*This style for nav should be included in Chimera */}
+        <nav className='flex gap-4'>
+          <a>ChimeraCSS</a>
+          <a>Guide</a>
+          <a>Documentation</a>
+          <a>Open Source</a>
+          <a>Contact us</a>
+        </nav>
+      </header>
+      <main className='flex justify-center'>
+        <div>
+          <h1>This is <a>ChimeraCSS</a></h1>
+          <p>The classless CSS-framework for clean, modern and soft design.</p>
+        </div>
+      </main>
+    </>
   )
 }
