a9968d0d43cc34fd277c3c565e64831021c3673b
Markus A. R. Johansen <90006516+J0hans1@users.noreply.github.com>
2023-08-23 23:25:31 +0200

    Chimera logo fix

--- src/app/components/ChimeraLogo.tsx
+++ src/app/components/ChimeraLogo.tsx
@@ -2,8 +2,8 @@
 
 const ChimeraLogo = () => {
     return (
-        <div className="animate-pulse w-60">
-            <img src="img/logo.svg" alt="Chimera" />
+        <div className="animate-pulse w-40">
+            <img src="/logo.svg" alt="Chimera" />
             {/* <img src="https://avatars.githubusercontent.com/u/141823796?s=96&v=4" alt="Chimera" /> */}
         </div>
     );
